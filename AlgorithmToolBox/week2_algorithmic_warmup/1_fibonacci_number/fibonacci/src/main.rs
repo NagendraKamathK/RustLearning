a56c66c473be17a4f94dd4354fdf1e4b9fc44c10
Nagendra Kamath K <knagendrakamath@gmail.com>
2024-10-17 07:05:15 +0530

    fibonacci iterative implemented

--- AlgorithmToolBox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci/src/main.rs
+++ AlgorithmToolBox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci/src/main.rs
@@ -1,8 +1,20 @@
 fn main() {
-    println!("{0}",fib(3));
+    let mut buff = String::new();
+    ::std::io::stdin().read_line(&mut buff);
+    let mut words = buff.split_whitespace();
+    let a: u32 = words.next().unwrap().parse().unwrap();
+
+    println!("{0}",fib(a));
 }
 
-fn fib(n:i32)->i32{
-    if n <=1 {return n}        
-    fib(n-2)+fib(n-1)
+fn fib(n:u32)->u32{
+    let mut n_2:u32 = 0;
+    let mut n_1:u32 = 1;
+  for i in 1..n
+  {
+      let temp = n_2;
+      n_2 = n_1;
+      n_1 = temp+n_2;
+  }
+    n_1
 }
